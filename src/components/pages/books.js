5e789f4af7101c4c1ad6c67d9ed1a6e71523ab39
Tiago Rahal <77216006+tiagorahal@users.noreply.github.com>
2021-08-31 12:30:04 -0300

    Update books.js

--- src/components/pages/books.js
+++ src/components/pages/books.js
@@ -1,7 +1,7 @@
 import Book from '../books/book';
-import BookNew from '../books/bookNew';
+import BookNew from '../books/booknew';
 
-import bookList from '../../lists/bookList';
+import bookList from '../../lists/booklist';
 
 const allBooks = bookList.map((book) => <Book key={book.id} info={book} />);
 
